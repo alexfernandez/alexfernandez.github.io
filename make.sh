118782e450cdc70dbc2de0109b535462c08e0c7d
Alex Fernández <alex.fernandez@mediasmart.es>
2015-10-04 23:43:22 +0200

    Make on 2015-10-04T21:43:22+0000

--- make.sh
+++ make.sh
@@ -3,8 +3,8 @@
 
 date=$(date -Iseconds -u)
 
-pandoc -w html5 -s index.md -o index.html
-pandoc -w html5 -s from-ground-to-cloud.md -o from-ground-to-cloud.html
+pandoc -w html5 -s md/index.md -o index.html
+pandoc -w html5 -s md/from-ground-to-cloud.md -o from-ground-to-cloud.html
 
 git add -A
 git commit -am "Make on $date"
