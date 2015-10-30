075dffd25c91d5bdbd57effa61e8e345cf3413fd
Alex Fernández <alex.fernandez@mediasmart.es>
2015-10-30 19:03:31 +0100

    Make on 2015-10-30T19:03:30Z

--- make.sh
+++ make.sh
@@ -6,22 +6,22 @@ date=$(date +"%Y-%m-%dT%H:%M:%SZ")
 # English
 pandoc --template read-only.html -s md/index.md -o index.html
 sed -i -e 's/"\.\.\//"/g' index.html
-pandoc --template read-only.html -s md/walk-through-my-code.md -o 2015/walk-through-my-code.html
-pandoc --template read-only.html -s md/iterative-evolution.md -o 2012/iterative-evolution.html
-pandoc --template read-only.html -s md/sources-of-irreversibility.md -o 2012/sources-of-irreversibility.html
-pandoc --template read-only.html -s md/performance-review.md -o 2012/performance-review.html
-pandoc --template read-only.html -s md/from-ground-to-cloud.md -o 2012/from-ground-to-cloud.html
-pandoc --template read-only.html -s md/closing-the-gap.md -o 2012/closing-the-gap.html
-pandoc --template read-only.html -s md/ode-to-telecommuting.md -o 2012/ode-to-telecommuting.html
-pandoc --template read-only.html -s md/reversible-engineering-part-3.md -o 2012/reversible-engineering-part-3.html
-pandoc --template read-only.html -s md/reversible-engineering-part-2.md -o 2012/reversible-engineering-part-2.html
-pandoc --template read-only.html -s md/reversible-engineering-part-1.md -o 2012/reversible-engineering-part-1.html
-pandoc --template read-only.html -s md/locked-to-the-cloud.md -o 2012/locked-to-the-cloud.html
-pandoc --template read-only.html -s md/continuous-deployment.md -o 2012/continuous-deployment.html
 pandoc --template read-only.html -s md/developer-discipline.md -o 2012/developer-discipline.html
+pandoc --template read-only.html -s md/continuous-deployment.md -o 2012/continuous-deployment.html
+pandoc --template read-only.html -s md/locked-to-the-cloud.md -o 2012/locked-to-the-cloud.html
+pandoc --template read-only.html -s md/reversible-engineering-part-1.md -o 2012/reversible-engineering-part-1.html
+pandoc --template read-only.html -s md/reversible-engineering-part-2.md -o 2012/reversible-engineering-part-2.html
+pandoc --template read-only.html -s md/reversible-engineering-part-3.md -o 2012/reversible-engineering-part-3.html
+pandoc --template read-only.html -s md/from-ground-to-cloud.md -o 2012/from-ground-to-cloud.html
+pandoc --template read-only.html -s md/ode-to-telecommuting.md -o 2012/ode-to-telecommuting.html
+pandoc --template read-only.html -s md/closing-the-gap.md -o 2012/closing-the-gap.html
+pandoc --template read-only.html -s md/performance-review.md -o 2012/performance-review.html
+pandoc --template read-only.html -s md/sources-of-irreversibility.md -o 2012/sources-of-irreversibility.html
+pandoc --template read-only.html -s md/iterative-evolution.md -o 2012/iterative-evolution.html
 pandoc --template read-only.html -s md/reporting-problems-part-1.md -o 2012/reporting-problems-part-1.html
 pandoc --template read-only.html -s md/reporting-problems-part-2.md -o 2012/reporting-problems-part-2.html
 pandoc --template read-only.html -s md/human-body-engineered-system.md -o 2013/human-body-engineered-system.html
+pandoc --template read-only.html -s md/walk-through-my-code.md -o 2015/walk-through-my-code.html
 
 # Español
 pandoc --template read-only.html -s md/optimizando-sockets.md -o 2013/optimizando-sockets.html
