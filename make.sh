05d25a98bd9ba3bb6ff46297fdb45a1bb94e0d49
Alex Fernandez <alex.fernandez@mediasmart.es>
2016-01-04 14:09:51 +0100

    Moved markdown to each year.

--- make.sh
+++ make.sh
@@ -4,36 +4,36 @@
 date=$(date +"%Y-%m-%dT%H:%M:%SZ")
 
 # English
-pandoc --template read-only.html -s md/index.md -o index.html
+pandoc --template read-only.html -s index.md -o index.html
 sed -i -e 's/"\.\.\//"/g' index.html
-pandoc --template read-only.html -s md/developer-discipline.md -o 2012/developer-discipline.html
-pandoc --template read-only.html -s md/continuous-deployment.md -o 2012/continuous-deployment.html
-pandoc --template read-only.html -s md/locked-to-the-cloud.md -o 2012/locked-to-the-cloud.html
-pandoc --template read-only.html -s md/reversible-engineering-part-1.md -o 2012/reversible-engineering-part-1.html
-pandoc --template read-only.html -s md/reversible-engineering-part-2.md -o 2012/reversible-engineering-part-2.html
-pandoc --template read-only.html -s md/reversible-engineering-part-3.md -o 2012/reversible-engineering-part-3.html
-pandoc --template read-only.html -s md/from-ground-to-cloud.md -o 2012/from-ground-to-cloud.html
-pandoc --template read-only.html -s md/ode-to-telecommuting.md -o 2012/ode-to-telecommuting.html
-pandoc --template read-only.html -s md/closing-the-gap.md -o 2012/closing-the-gap.html
-pandoc --template read-only.html -s md/performance-review.md -o 2012/performance-review.html
-pandoc --template read-only.html -s md/sources-of-irreversibility.md -o 2012/sources-of-irreversibility.html
-pandoc --template read-only.html -s md/iterative-evolution.md -o 2012/iterative-evolution.html
-pandoc --template read-only.html -s md/reporting-problems-part-1.md -o 2012/reporting-problems-part-1.html
-pandoc --template read-only.html -s md/reporting-problems-part-2.md -o 2012/reporting-problems-part-2.html
-pandoc --template read-only.html -s md/human-body-engineered-system.md -o 2013/human-body-engineered-system.html
-pandoc --template read-only.html -s md/walk-through-my-code.md -o 2015/walk-through-my-code.html
-pandoc --template read-only.html -s md/the-intelligent-designer.md -o 2015/the-intelligent-designer.html
-pandoc --template read-only.html -s md/entrevista-devuse.md -o 2015/entrevista-devuse.html
-pandoc --template read-only.html -s md/arquitectura-fluida-1-arquitectura-perfecta.md -o 2015/arquitectura-fluida-1-arquitectura-perfecta.html
-pandoc --template read-only.html -s md/arquitectura-fluida-2-estrategias-migracion.md -o 2015/arquitectura-fluida-2-estrategias-migracion.html
+pandoc --template read-only.html -s 2012/developer-discipline.md -o 2012/developer-discipline.html
+pandoc --template read-only.html -s 2012/continuous-deployment.md -o 2012/continuous-deployment.html
+pandoc --template read-only.html -s 2012/locked-to-the-cloud.md -o 2012/locked-to-the-cloud.html
+pandoc --template read-only.html -s 2012/reversible-engineering-part-1.md -o 2012/reversible-engineering-part-1.html
+pandoc --template read-only.html -s 2012/reversible-engineering-part-2.md -o 2012/reversible-engineering-part-2.html
+pandoc --template read-only.html -s 2012/reversible-engineering-part-3.md -o 2012/reversible-engineering-part-3.html
+pandoc --template read-only.html -s 2012/from-ground-to-cloud.md -o 2012/from-ground-to-cloud.html
+pandoc --template read-only.html -s 2012/ode-to-telecommuting.md -o 2012/ode-to-telecommuting.html
+pandoc --template read-only.html -s 2012/closing-the-gap.md -o 2012/closing-the-gap.html
+pandoc --template read-only.html -s 2012/performance-review.md -o 2012/performance-review.html
+pandoc --template read-only.html -s 2012/sources-of-irreversibility.md -o 2012/sources-of-irreversibility.html
+pandoc --template read-only.html -s 2012/iterative-evolution.md -o 2012/iterative-evolution.html
+pandoc --template read-only.html -s 2012/reporting-problems-part-1.md -o 2012/reporting-problems-part-1.html
+pandoc --template read-only.html -s 2012/reporting-problems-part-2.md -o 2012/reporting-problems-part-2.html
+pandoc --template read-only.html -s 2013/human-body-engineered-system.md -o 2013/human-body-engineered-system.html
+pandoc --template read-only.html -s 2015/walk-through-my-code.md -o 2015/walk-through-my-code.html
+pandoc --template read-only.html -s 2015/the-intelligent-designer.md -o 2015/the-intelligent-designer.html
+pandoc --template read-only.html -s 2015/entrevista-devuse.md -o 2015/entrevista-devuse.html
+pandoc --template read-only.html -s 2015/arquitectura-fluida-1-arquitectura-perfecta.md -o 2015/arquitectura-fluida-1-arquitectura-perfecta.html
+pandoc --template read-only.html -s 2015/arquitectura-fluida-2-estrategias-migracion.md -o 2015/arquitectura-fluida-2-estrategias-migracion.html
 
 # Español
-pandoc --template read-only.html -s md/optimizando-sockets.md -o 2013/optimizando-sockets.html
-pandoc --template read-only.html -s md/pruebas-de-carga.md -o 2013/pruebas-de-carga.html
-pandoc --template read-only.html -s md/nodejs-rapido-como-el-rayo.md -o 2013/nodejs-rapido-como-el-rayo.html
-pandoc --template read-only.html -s md/modo-cluster.md -o 2013/modo-cluster.html
-pandoc --template read-only.html -s md/pruebas-asincronas.md -o 2013/pruebas-asincronas.html
-pandoc --template read-only.html -s md/ofertas-salarios.md -o 2015/ofertas-salarios.html
+pandoc --template read-only.html -s 2013/optimizando-sockets.md -o 2013/optimizando-sockets.html
+pandoc --template read-only.html -s 2013/pruebas-de-carga.md -o 2013/pruebas-de-carga.html
+pandoc --template read-only.html -s 2013/nodejs-rapido-como-el-rayo.md -o 2013/nodejs-rapido-como-el-rayo.html
+pandoc --template read-only.html -s 2013/modo-cluster.md -o 2013/modo-cluster.html
+pandoc --template read-only.html -s 2013/pruebas-asincronas.md -o 2013/pruebas-asincronas.html
+pandoc --template read-only.html -s 2015/ofertas-salarios.md -o 2015/ofertas-salarios.html
 
 git add -A
 git commit -am "Make on $date"
