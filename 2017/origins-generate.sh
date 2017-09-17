8a4785f0b5124617b10e574bbdb00d40e7d7a93e
Alex Fernández <alex.fernandez@mediasmart.es>
2017-09-17 02:07:27 +0200

    Make on 2017-09-17T02:07:24Z

--- 2017/origins-generate.sh
+++ 2017/origins-generate.sh
@@ -1,12 +1,16 @@
 # Alex 2017-09-09
 # Generate PDF for The Origins of Language.
-# Follow advice in
-# https://tex.stackexchange.com/questions/64802/the-character-%C9%99-in-plain-tex:
-# Insert the following lines before the first schwa:
-# \font\tenipa=tipa10
-# \def\schwa{{\tenipa\char64}}
-# Then replace all Ə by \schwa
-# Finally run the following command inside the 2017/ directory:
+# First convert to latex:
 
-pandoc --latex-engine=xelatex -s origins-language.md -o origins-language.pdf --variable=documentclass:article
+pandoc --latex-engine=xelatex -s origins-language.md -o origins-language.latex --variable=documentclass:article
+
+# Then Follow advice in
+# https://tex.stackexchange.com/questions/357616/how-to-insert-the-letters-%C6%9F-%C6%A2-%C6%8F-%CA%90-%D2%9A-and-%D2%A2,
+# but with Charis SIL font.
+# Place the following two lines after the first `\fi`:
+#   \usepackage{fontspec}
+#   \setmainfont{Charis SIL}
+# And run again:
+
+xelatex origins-language.latex
 
