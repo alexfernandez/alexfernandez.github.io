764d119156d79274d7b76bd312f58814db4a97de
Alex Fernández <alejandrofer@gmail.com>
2019-12-28 11:10:18 +0100

    Make on 2019-12-28T11:10:12Z

--- make.sh
+++ make.sh
@@ -44,6 +44,7 @@ pandoc --template read-only.html -s 2018/more-golang-adventures.md -o 2018/more-
 pandoc --template read-only.html -s 2018/has-anyone-else-seen-your-code.md -o 2018/has-anyone-else-seen-your-code.html
 pandoc --template read-only.html -s 2019/ethics-for-techies.md -o 2019/ethics-for-techies.html
 pandoc --template read-only.html -s 2019/high-speeds.md -o 2019/high-speeds.html
+pandoc --template read-only.html -s 2019/devops-on-the-cheap.md -o 2019/devops-on-the-cheap.html
 pandoc --template read-only.html -s permanent/cv.md -o permanent/cv.html
 pandoc -s permanent/standalone-cv.md -o permanent/cv-alex-fernandez.pdf
 pandoc --template read-only.html -s permanent/speaker.md -o permanent/speaker.html
